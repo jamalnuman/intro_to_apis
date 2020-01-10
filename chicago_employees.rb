96eaeff137871c3abf03735593c943a377b991c5
“YOUR <jnuman82@gmail.com>
2020-01-10 10:56:16 -0600

    updating content

--- chicago_employees.rb
+++ chicago_employees.rb
@@ -1,4 +1,4 @@
-#to use a gem, you have to 
+#to use a gem, you have to require the gem
 require 'http' #the require argument needs to be in quotation marks 
 
 response = HTTP.get("https://data.cityofchicago.org/resource/xzkq-xp2w.json")
@@ -13,9 +13,37 @@ puts
 
 if employee['salary_or_hourly'] == "Salary"
   puts "Annual Salary: #{employee["annual_salary"]}"
+elsif employee['salary_or_hourly'] == "Hourly"
+  puts "Hourly Rate: $#{employee["hourly_rate"]}"
 end
 
 
   p employee
   puts "=" * 30
 end
+
+# require 'http'
+
+# response = HTTP.get("https://data.cityofchicago.org/resource/xzkq-xp2w.json")
+# employees = response.parse
+
+# employees.each do |employee|
+#   puts "Name: #{employee["name"]}"
+#   puts "Job: #{employee["job_titles"]}"
+#   puts "Dept: #{employee["department"]}"
+
+#   puts
+
+#   if employee["salary_or_hourly"] == "Salary"
+#     puts "Salary: $#{employee["annual_salary"]}"
+#   else
+#     puts "Typical Hours: #{employee["typical_hours"]}"
+#     puts "Hourly Rate: #{employee["hourly_rate"]}"
+#     rate_per_week = employee["typical_hours"].to_i * employee["hourly_rate"].to_f
+#     puts "Estimated Salary: $#{ rate_per_week * 50 }"
+#   end
+
+#   puts
+#   puts "=" * 30
+#   puts
+# end
